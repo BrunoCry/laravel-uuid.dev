d633e3bcf2b0db37d478b08aa7df4c703dd7d970
BrunoCry <danil-az2@mail.ru>
2023-11-02 16:23:58 +0300

    Include navbar

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>Test</div>
+    <navigation-bar />
+
+    <div class="container mt-4">
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
+    import NavigationBar from './components/NavigationBar.vue'
 
+    export default {
+        components: {
+            NavigationBar
+        }
+    }
 </script>
