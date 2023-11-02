896a8986563286c5cb40e4c24df46bfaa7bb493c
BrunoCry <danil-az2@mail.ru>
2023-11-02 16:23:48 +0300

    Add navigation bar component

---
+++ resources/js/components/NavigationBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">{{ appName }}</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link" exact-active-class="active">Home</router-link>
+                    </li>
+
+                    <li class="nav-item">
+                        <router-link to="/generate" class="nav-link" exact-active-class="active">Generate</router-link>
+                    </li>
+
+                    <li class="nav-item">
+                        <router-link to="/retrieve" class="nav-link" exact-active-class="active">Retrieve</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            appName: import.meta.env.VITE_APP_NAME
+        })
+    }
+</script>
