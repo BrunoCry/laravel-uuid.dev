93131008a071842c45276d7a177abdf6aaa77740
BrunoCry <danil-az2@mail.ru>
2023-11-02 21:29:06 +0300

    Check status code in error function

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -1,4 +1,8 @@
 export function showError (error) {
+    if (error.response && error.response.status == 404) {
+        return
+    }
+
     alert('Произошла ошибка при выполнении запроса. Проверьте консоль.')
     throw new Error(error)
 }
