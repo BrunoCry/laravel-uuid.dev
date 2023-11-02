ccd7af49fb7a08b37194b19c0789f534a099d8e6
BrunoCry <danil-az2@mail.ru>
2023-11-02 15:19:01 +0300

    Routing for UUID API

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\API\UUIDController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'uuid'], function () {
+    Route::get('list', [UUIDController::class, 'list'])->name('uuid.list');
+    Route::post('generate', [UUIDController::class, 'generate'])->name('uuid.generate');
+    Route::get('retrieve/{id}', [UUIDController::class, 'retrieve'])->name('uuid.retrieve');
 });
